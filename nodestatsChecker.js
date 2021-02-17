b7e2a2d427907a1e15e3bd86d65810d122905ece
Seekedd <75855733+Seekedd@users.noreply.github.com>
2021-02-17 19:18:34 +0100

    Add files via upload

---
+++ nodestatsChecker.js
@@ -0,0 +1,260 @@
+const axios = require('axios');
+var ping = require('ping');
+const ping2 = require('ping-tcp-js')
+
+let stats = {
+    node1: {
+        serverID: '7c740e8c',
+        IP: '142.54.191.91'
+    },
+    node2: {
+        serverID: '4bbbdb0d',
+        IP: '142.54.191.93'
+    },
+    node3: {
+        serverID: '57622dc8',
+        IP: '167.86.113.158'
+    },
+    node4: {
+        serverID: '98ca4dbd',
+        IP: '178.159.3.233'
+    },
+    node5: {
+        serverID: '6d83e569',
+        IP: '154.27.68.108'
+    },
+    node6: {
+        serverID: '8565f2e0',
+        IP: '194.146.44.129'
+    },
+    node7: {
+        serverID: '94082df3',
+        IP: '142.54.191.92'
+    },
+    node8: {
+        serverID: '8e1d9c32',
+        IP: '194.146.44.168'
+    },
+    node9: {
+        serverID: 'a0493565',
+        IP: '194.146.44.41'
+    },
+    node10: {
+        serverID: '3c93984c',
+        IP: '194.146.44.58'
+    },
+    node11: {
+        serverID: '0267242c',
+        IP: '178.159.2.82'
+    },
+    node12: {
+        serverID: '4526d28b',
+        IP: '178.159.2.89'
+    },
+    node13: {
+        serverID: '8bb3a785',
+        IP: '178.159.3.205'
+    },
+    node14: {
+        serverID: '7ad8c41b',
+        IP: '194.146.44.188'
+    },
+
+    storage1: {
+        serverID: 'b2af1392',
+        IP: '194.146.44.73'
+    },
+}
+
+console.log(chalk.magenta('[Nodes Checker] ') + chalk.green("Online"));
+//Node status 
+setInterval(() => {
+    //console.log("Checking Nodes...");
+
+    //Public nodes
+    for (let [node, data] of Object.entries(stats)) {
+        axios({
+            url: config.Pterodactyl.hosturl + "/api/client/servers/" + data.serverID + "/resources",
+            method: 'GET',
+            followRedirect: true,
+            maxRedirects: 5,
+            headers: {
+                'Authorization': 'Bearer ' + config.Pterodactyl.apikeyclient,
+                'Content-Type': 'application/json',
+                'Accept': 'Application/vnd.pterodactyl.v1+json',
+            }
+        }).then(response => {
+            nodeStatus.set(node, {
+                status: true,
+                is_vm_online: true
+            });
+        }).catch(error => {
+
+            axios({
+                url: 'http://' + data.IP + ':999/random',
+                timeout: 1500,
+            }).then(x => {
+
+            }).catch(x => {
+
+                if (x.response != null) {
+                    nodeStatus.set(node, {
+                        status: false,
+                        is_vm_online: true
+                    });
+                } else {
+                    nodeStatus.set(node, {
+                        status: false,
+                        is_vm_online: false
+                    });
+                }
+            });
+        })
+    }
+
+    //Server limit
+
+
+    //Node servers checker
+    axios({
+        url: config.PrivPterodactyl.hosturl + "/api/client/servers/88a20baf/resources",
+        method: 'GET',
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+            'Authorization': 'Bearer ' + config.PrivPterodactyl.apikeyclient,
+            'Content-Type': 'application/json',
+            'Accept': 'Application/vnd.pterodactyl.v1+json',
+        }
+    }).then(response => {
+        nodeStatus.set("node1-priv", {
+            status: true
+        });
+    }).catch(error => {
+        nodeStatus.set("node1-priv", {
+            status: false
+        });
+    })
+
+    //Node 1 (PRIVATE ADMIN PANEL)
+    axios({
+        url: config.PrivPterodactyl.hosturl + "/api/client/servers/6634ce4f/resources",
+        method: 'GET',
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+            'Authorization': 'Bearer ' + config.PrivPterodactyl.apikeyclient,
+            'Content-Type': 'application/json',
+            'Accept': 'Application/vnd.pterodactyl.v1+json',
+        }
+    }).then(response => {
+        nodeStatus.set("node1-priv", {
+            status: true
+        });
+    }).catch(error => {
+        nodeStatus.set("node1-priv", {
+            status: false
+        });
+    })
+
+    //Node 1
+    axios({
+        url: config.DanPterodactyl.hosturl + "/api/client/servers/019b6467/resources",
+        method: 'GET',
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+            'Authorization': 'Bearer ' + config.DanPterodactyl.apikeyclient,
+            'Content-Type': 'application/json',
+            'Accept': 'Application/vnd.pterodactyl.v1+json',
+        }
+    }).then(response => {
+        nodeStatus.set("dan-node1", {
+            status: true
+        });
+    }).catch(error => {
+        nodeStatus.set("dan-node1", {
+            status: false
+        });
+    })
+
+    // Panel Cherckers
+    var hosts = ['63.141.228.92', 'panel.danbot.host', 'mail.danbot.host', 'api.danbot.host', 'admin.danbot.host', 'private.danbot.host'];
+    hosts.forEach(function (host) {
+        ping.sys.probe(host, function (isAlive) {
+            if (isAlive == true) {
+                nodeStatus.set(host, {
+                    status: true
+                })
+            } else if (isAlive == false) {
+                nodeStatus.set(host, {
+                    status: false
+                });
+            }
+        });
+    }, {
+        timeout: 4
+    });
+
+    //Panel stuffs
+    ping2.ping('157.230.202.210', 80) // Panel 1
+        .then(() => nodeStatus.set("panelus", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("panelus", {
+            status: false
+        }));
+    ping2.ping('208.68.39.241', 80) // Panel 1
+        .then(() => nodeStatus.set("panelus1", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("panelus1", {
+            status: false
+        }));
+    ping2.ping('104.248.225.46', 80) //Panel 2
+        .then(() => nodeStatus.set("panelus2", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("panelus2", {
+            status: false
+        }));
+    ping2.ping('134.122.112.51', 80) //Panel 3
+        .then(() => nodeStatus.set("panelus3", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("panelus3", {
+            status: false
+        }));
+    ping2.ping('104.248.237.198', 80) //Panel 4
+        .then(() => nodeStatus.set("panelus4", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("panelus4", {
+            status: false
+        }));
+    ping2.ping('161.35.138.206', 3306)
+        .then(() => nodeStatus.set("dbhdb", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("dbhdb", {
+            status: false
+        }));
+
+    //Lavalink chercker
+    ping2.ping('lava.danbot.host', 2333)
+        .then(() => nodeStatus.set("lava.danbot.host", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("lava.danbot.host", {
+            status: false
+        }));
+
+    ping2.ping('lava2.danbot.host', 2333)
+        .then(() => nodeStatus.set("lava2.danbot.host", {
+            status: true
+        }))
+        .catch((e) => nodeStatus.set("lava2.danbot.host", {
+            status: false
+        }));
+
+}, 3000)
---
+++ routes/me.js
@@ -0,0 +1,158 @@
+// route for users to get their own bots
+const ms = require('ms')
+const Router = require("express").Router();
+const db = require("quick.db");
+const isSnowflake = require(process.cwd() + "/util/isSnowflake.js");
+
+Router.get("/", checkAuth, (req, res) => {
+  let bots = db.get(`${req.user.id}.bots`);
+  res.render("me/index.ejs", {
+    user: req.isAuthenticated() ? req.user : null,
+    bots,
+    db
+  });
+});
+
+Router.get("/form/new-server", checkAuth, (req, res) => {
+  let bots = db.get(`${req.user.id}.bots`);
+  res.render("forms/newserver.ejs", {
+    user: req.isAuthenticated() ? req.user : null,
+    bots,
+    db
+  });
+});
+
+Router.post("/form/new-server", checkAuth, (req, res) => {
+
+  let data = req.body;
+  console.log(data);
+  res.redirect("/me?e=ERROR");
+
+});
+
+Router.get("/form/staff-apply", checkAuth, (req, res) => {
+  let bots = db.get(`${req.user.id}.bots`);
+  if (webSettings.fetch("staff-applications.enabled") == "true") {
+    res.render("forms/apply-staff.ejs", {
+      user: req.isAuthenticated() ? req.user : null,
+      bots,
+      db
+    });
+  } else if (webSettings.fetch("staff-applications.enabled") == "false") {
+    res.render("forms/apply-staff-closed.ejs", {
+      user: req.isAuthenticated() ? req.user : null,
+      bots,
+      db
+    });
+  }
+});
+
+Router.post("/form/staff-apply", checkAuth, (req, res) => {
+
+  let data = req.body;
+  data.member = client.guilds.cache.get('639477525927690240').members.cache.get(data.user);
+
+  if (data.member == null) return res.send({
+    error: "You're not a member in out discord server, make sure to join before applying."
+  });
+  let channel = client.channels.cache.get("786607811870064650");
+  try {
+
+    const embed = new Discord.MessageEmbed()
+      .setColor(0x00A2E8)
+      .addField("__**Ping**__", data.member)
+      .addField("__**User ID**__", '`<@'+data.user +'>`')
+      .addField("__**age**__", data.age)
+      .addField("__**Console Email**__", data.cemail)
+      .addField("__**How long have you been in DBH?**__", ms(Date.now() - Date.parse(data.member.joinedAt), {
+        long: true
+      }))
+      .addField("__**Languages**__", data.langs)
+      .addField("__**Previous experiences**__", data.prev)
+      .addField("__**Coding knowledge**__", data.coding)
+      .addField("__**Any projects you are proud of?**__", data.projects)
+      .addField("__**Anything else**__", data.else)
+      .setTimestamp()
+      .setFooter("New staff app submitted!");
+    channel.send({
+      embed
+    })
+  } catch (error) {
+
+
+    let toSend = ""
+
+    toSend += '__**Ping**__\n' + data.member;
+    toSend += '\n-------------------------------------\n__**User ID**__\n' + data.member;
+    toSend += '\n-------------------------------------\n__**User ID**__\n' + '`<@'+data.user +'>`';
+    toSend += '\n-------------------------------------\n__**age**__\n' + data.age;
+    toSend += '\n-------------------------------------\n__**Console Email**__\n' + data.cemail;
+    toSend += '\n-------------------------------------\n__**How long have you been in DBH?**__\n' + ms(Date.now() - Date.parse(data.member.joinedAt), {
+      long: true
+    });
+
+    toSend += '\n-------------------------------------\n__**Languages**__\n' + data.langs;
+    toSend += '\n-------------------------------------\n__**Previous experiences**__\n' + data.prev;
+    toSend += '\n-------------------------------------\n__**Coding knowledge**__\n' + data.coding;
+    toSend += '\n-------------------------------------\n__**Any projects you are proud of?**__\n' + data.projects;
+    toSend += '\n-------------------------------------\n__**Anything else**__\n' + data.else;
+
+
+    channel.send(toSend).catch(y => {
+      const att = new Discord.Attachment(Buffer.from(toSend), data.member.user.tag + ' - ' + data.member.id + '.txt');
+      channel.send(data.member, att);
+    })
+
+
+  }
+
+  res.redirect("/me?e=COMPLETE");
+
+});
+
+Router.get("/requests", checkAuth, (req, res) => {
+  let bots = db.get(`${req.user.id}.bots`);
+  res.render("requests.ejs", {
+    user: req.isAuthenticated() ? req.user : null,
+    bots,
+    db
+  });
+});
+
+// Router.post("/requests", checkAuth, (req, res) => {
+
+//   let data = req.body;
+//   console.log(data);
+//   const embed = new Discord.RichEmbed()
+//     .setColor(0x00A2E8)
+//     .addField("__**Ping**__", `<@${data.id}>`)
+//     .addField("__**User ID**__", data.id)
+//     .addField("__**Console Email**__", data.cemail)
+//     .addField("__**How long have you been in DBH?**__", data.joindate)
+//     .addField("__**Previous experiences**__", data.prev)
+//     .addField("__**Coding knowledge**__", data.coding)
+//     .addField("__**Any projects you are proud of?**__", data.projects)
+//     .setTimestamp()
+//     .setFooter("New staff app submitted! ");
+//   client.channels.get("757204887242014760").send({
+//     embed
+//   });
+
+//   res.redirect("/me?e=COMPLETE");
+
+// });
+
+module.exports = Router;
+
+/*
+ * Authorization check, if not authorized return them to the login page.
+ */
+function checkAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.session.backURL = req.url;
+
+    res.redirect("/login?redirect=/me");
+  }
+}
